78f3115ca83b0f21779586833392e3ac04f02da1
Fernando Chiquete <l21170293@culiacan.tecnm.mx>
2026-05-23 17:45:03 -0700

    tabla mensaje

---
+++ src/main/java/com/whatsopp/whatsopp/models/ids/mensajeId.java
@@ -0,0 +1,44 @@
+package com.whatsopp.whatsopp.models.ids;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class mensajeId implements Serializable {
+    private Long contenido_id;
+    private Long remitente_id;
+    private Long destinatario_id;
+
+    public mensajeId(Long contenido_id, Long remitente, Long destinatario){
+        this.contenido_id=contenido_id;
+        this.remitente_id=remitente;
+        this.destinatario_id=destinatario;
+    }
+
+    public Long getContenido_id() {
+        return contenido_id;
+    }
+
+    public void setContenido_id(Long contenido_id) {
+        this.contenido_id = contenido_id;
+    }
+
+    public Long getRemitente() {
+        return remitente_id;
+    }
+
+    public void setRemitente(Long remitente) {
+        this.remitente_id = remitente;
+    }
+
+    public Long getDestinatario() {
+        return destinatario_id;
+    }
+
+    public void setDestinatario(Long destinatario) {
+        this.destinatario_id = destinatario;
+    }
+
+    
+}
--- src/main/java/com/whatsopp/whatsopp/models/mensaje.java
+++ src/main/java/com/whatsopp/whatsopp/models/mensaje.java
@@ -1,59 +1,19 @@
 package com.whatsopp.whatsopp.models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import com.whatsopp.whatsopp.models.ids.mensajeId;
+
 @Entity
 @Table(name="mensaje")
 public class mensaje {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long mensaje_id;
-
-    @Id
-    private Long mensaje_remitente;
-
-    @Id
-    private Long mensaje_destinatario;
-
-    @Column(nullable = false)
-    private Long mensaje_contenido;
-
-    public Long getMensaje_id() {
-        return mensaje_id;
-    }
-
-    public void setMensaje_id(Long mensaje_id) {
-        this.mensaje_id = mensaje_id;
-    }
-
-    public Long getMensaje_remitente() {
-        return mensaje_remitente;
-    }
-
-    public void setMensaje_remitente(Long mensaje_remitente) {
-        this.mensaje_remitente = mensaje_remitente;
-    }
-
-    public Long getMensaje_destinatario() {
-        return mensaje_destinatario;
-    }
-
-    public void setMensaje_destinatario(Long mensaje_destinatario) {
-        this.mensaje_destinatario = mensaje_destinatario;
-    }
-
-    public Long getMensaje_contenido() {
-        return mensaje_contenido;
-    }
-
-    public void setMensaje_contenido(Long mensaje_contenido) {
-        this.mensaje_contenido = mensaje_contenido;
-    }
-
+    @EmbeddedId
+    private mensajeId mensajeId;
     
 }
